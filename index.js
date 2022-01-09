531c8b1cf8bb76addc5d57a8dc9399394bee2206
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-09 20:03:17 +0100

    redux_multiple_action_in_on_reducer

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const createStore=redux.createStore
 
 
 const BUY_CAKE= 'BUY-CAKE'
+const BUY_ICECREAM= 'BUY-ICECREAM'
 
 const buyCake=()=>{
     return {
         type:BUY_CAKE,
     }
 }
+const buyIceCream=()=>{
+    return {
+        type:BUY_ICECREAM,
+    }
+}
+
 
 const initial={
-    numberOfCake:10
+    numberOfCake:10,
+    numberOfIceCream:10
 }
 
 const reducer=(state=initial,action)=>{
     switch (action.type) {
         case 'BUY-CAKE':
            return {...state,numberOfCake:state.numberOfCake-1 } 
+        case 'BUY-ICECREAM':
+           return {...state,numberOfIceCream:state.numberOfIceCream-1 } 
         default:
          return state;
     }
@@ -26,7 +36,7 @@ const reducer=(state=initial,action)=>{
 const store=createStore(reducer)
 console.log('Initial state' , store.getState());
 const unsbscribe= store.subscribe(()=>console.log('Update state', store.getState()))
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 store.dispatch(buyCake())
 unsbscribe()
